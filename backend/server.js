7702f33eebc48c615253957b60eca79515eef441
hamzaabbasi54 <hamza.abbasi3030@gmail.com>
2026-02-07 22:37:10 +0500

    checking if changes have been pushed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import authRoutes from './routes/auth.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,16 +13,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.get('/api/auth/signup', (req, res) => {
-    res.send("Signup");
-})
-app.get('/api/auth/login', (req, res) => {
-    res.send("Login");
-})
-app.get('/api/auth/logout', (req, res) => {
-    res.send("Logout");
-})
+const __dirname = path.resolve();
+app.use('/api/auth', authRoutes);
+//making it ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})  
